d5fb18d0c72b159316ee1b39f2af3b866a09204e
vilemsonsdavis <44113043+vilemsonsdavis@users.noreply.github.com>
2020-04-15 21:10:09 +0300

    moved to includes folder

--- dbh.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "phplessons";
-
-//this is how we connect to the db
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
